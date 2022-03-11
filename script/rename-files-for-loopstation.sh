c250099bc7288d37375509642e510a7a593625ef
frank <ianrobot@posteo.eu>
2022-03-11 23:03:26 +0100

    Allow default for omitted source dir arg and avoid recursion

--- script/rename-files-for-loopstation.sh
+++ script/rename-files-for-loopstation.sh
@@ -3,21 +3,23 @@
 set -ex
 
 # check args
-if [ ! -d "${1}" ]; then
-  printf "usage: %s AUDIO_FILES_DIR [TARGET_DIR]\n\n" "${0}"
-  printf "AUDIO_FILES_DIR is the directory where the audio files reside."
-  printf "TARGET_DIR is opional and is a directory where the renamed files are copied to. If it not exists it will be created."
-fi
+#if [ ! -d "${1}" ]; then
+#  printf "usage: %s [AUDIO_FILES_DIR] [TARGET_DIR]\n\n" "${0}"
+#  printf "AUDIO_FILES_DIR is the directory where the audio files reside."
+#  printf "TARGET_DIR is opional and is a directory where the renamed files are copied to. If it not exists it will be created."
+# fi
 
 
-SOURCE_DIR=$1
+SOURCE_DIR=${1:-'.'}
 TARGET_DIR=${2:-"Renamed Files"}
 
-[[ -d "${TARGET_DIR}" ]] ||  mkdir "${TARGET_DIR}"
 
 # TODO check files
 i=1
+# read file list before creating target dir to avoid recursion
 FILES=$(ls "${SOURCE_DIR}")
+[[ -d "${TARGET_DIR}" ]] ||  mkdir "${TARGET_DIR}"
+
 for file in $FILES; do
   #
   NEW_FILE="${TARGET_DIR}"/$(echo "${file}" | sed -E "s/[^\.]*\.(.+)/${i}.\1/g")
